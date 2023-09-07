af85c5cd5d1020818a908d797351e19097c7ff99
sprollucy <sprollucy@yahoo.com>
2023-09-07 01:19:43 -0400

    dftgthr

--- UiBot/AboutForm.cs
+++ UiBot/AboutForm.cs
@@ -13,38 +13,38 @@ using System.Threading.Tasks;
 namespace UiBot
 {
     internal class AboutForm
-        {
+    {
 
 
-            InitializeComponent();
+        InitializeComponent();
             this.FormBorderStyle = FormBorderStyle.FixedSingle;
         }
 
-        private void AboutForm_Load(object sender, EventArgs e)
-        {
-            label1.Text = "Version: " + Application.ProductVersion;
-        }
-
-        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-            // Specify the URL you want to open
-            string url = "https://github.com/sprollucy/Tarkov-Twitch-Bot-Working";
-
-            // Open the URL in the default web browser
-            System.Diagnostics.Process.Start(new ProcessStartInfo(url)
-            {
-                UseShellExecute = true
-            });
-        }
-
-        private void groupBox1_Enter(object sender, EventArgs e)
-        {
-
-        }
-
-        private void richTextBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
+    private void AboutForm_Load(object sender, EventArgs e)
+    {
+        label1.Text = "Version: " + Application.ProductVersion;
     }
+
+    private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+    {
+        // Specify the URL you want to open
+        string url = "https://github.com/sprollucy/Tarkov-Twitch-Bot-Working";
+
+        // Open the URL in the default web browser
+        System.Diagnostics.Process.Start(new ProcessStartInfo(url)
+        {
+            UseShellExecute = true
+        });
+    }
+
+    private void groupBox1_Enter(object sender, EventArgs e)
+    {
+
+    }
+
+    private void richTextBox2_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+}
 }
